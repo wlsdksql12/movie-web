da6ecc65080678dd3ff104f30e270fd8bdaf8e56
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-08-25 17:04:34 +0900

    props 복습

--- js/index.js
+++ js/index.js
@@ -1,119 +1,37 @@
 const root = document.getElementById("root");
-
-function MinutesToHours() {
-  const [amount, setAmount] = React.useState();
-  const [MinutesToHours, setMinutesToHours] = React.useState(false);
-  const onChange = (e) => {
-    setAmount(e.target.value);
-  };
-  const reset = (e) => {
-    setAmount(0);
-  };
-  const onMinutesToHours = (e) => {
-    reset();
-    setMinutesToHours((current) => !current);
-  };
+function Btn({ text, onClick }) {
+  console.log(text, "wasrendered");
   return (
-    <div>
-      <h1>Minutes & Hours</h1>
-      <div>
-        <label htmlFor="minutes">Minutes: </label>
-        <input
-          id="minutes"
-          placeholder="Minutes"
-          type="number"
-          value={MinutesToHours ? amount * 60 : amount}
-          onChange={onChange}
-          disabled={MinutesToHours}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="hours">Hours: </label>
-        <input
-          id="hours"
-          placeholder="Hours"
-          type="number"
-          value={MinutesToHours ? amount : Math.round(amount / 60)}
-          onChange={onChange}
-          disabled={!MinutesToHours}
-        />
-      </div>
-      <button onClick={reset}>Reset</button>
-      <button onClick={onMinutesToHours}>
-        {MinutesToHours ? "Minutes" : "Hours"}
-      </button>
-    </div>
+    <button
+      onClick={onClick}
+      style={{
+        backgroundColor: "tomato",
+        color: "white",
+        padding: "10px 20px",
+        border: 0,
+        borderRadius: 10,
+      }}
+    >
+      {text}
+    </button>
   );
 }
-function KmToMiles() {
-  const [amount, setAmount] = React.useState();
-  const [KmToMiles, setKmToMiles] = React.useState(false);
-  const onChange = (e) => {
-    setAmount(e.target.value);
-  };
-  const reset = (e) => {
-    setAmount(0);
-  };
-  const onKmToMiles = (e) => {
-    reset();
-    setKmToMiles((current) => !current);
-  };
-  return (
-    <div>
-      <h1>Km & Miles</h1>
-      <div>
-        <label htmlFor="minutes">Km: </label>
-        <input
-          id="Km"
-          placeholder="Km"
-          type="number"
-          value={KmToMiles ? amount * 1.61 : amount}
-          onChange={onChange}
-          disabled={KmToMiles}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="hours">Miles: </label>
-        <input
-          id="Miles"
-          placeholder="Miles"
-          type="number"
-          value={KmToMiles ? amount : Math.round(amount / 1.61)}
-          onChange={onChange}
-          disabled={!KmToMiles}
-        />
-      </div>
-      <button onClick={reset}>Reset</button>
-      <button onClick={onKmToMiles}>{KmToMiles ? "Km" : "Miles"}</button>
-    </div>
-  );
-}
+// const MemorizedBtn = React.memo(Btn);
+Btn.propTypes = {
+  text: PropTypes.number,
+  onClick: PropTypes.number,
+};
 function App() {
-  const [index, setIndex] = React.useState("xx");
-  const onSelect = (e) => {
-    setIndex(e.target.value);
+  const [value, setValue] = React.useState("Save Chanes");
+  const changeValue = () => {
+    setValue("Revert Changes");
   };
   return (
     <div>
-      <h1>Super Converter</h1>
-      <select value={index} onChange={onSelect}>
-        <option value="xx">Select your units</option>
-        <option value="0">Minutes & Hours</option>
-        <option value="1">Km & Miles</option>
-      </select>
-      <hr />
-      {index === "0" ? <MinutesToHours /> : null}
-      {index === "1" ? <KmToMiles /> : null}
+      <Btn text={value} onClick={changeValue} />
+      <Btn text={"Confirm"} />
     </div>
   );
 }
 
 ReactDOM.render(<App />, root);
-
-// function render() {
-//   ReactDOM.render(<Container />, root);
-// }
-
-// const container = React.createElement("div", null, [Title, btn]);
