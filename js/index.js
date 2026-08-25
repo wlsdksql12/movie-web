70a382d86aca9a6f6806be76440974784f47fc64
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-08-25 16:13:18 +0900

    state 기능 공부 및 state를 활용하여 단위 변환기 완성

--- js/index.js
+++ js/index.js
@@ -1,49 +1,116 @@
 const root = document.getElementById("root");
-// const Title = () => (
-//   <span id="spanid">
-//     Hello React world
-//     <br />
-//   </span>
-// );
-// const span = React.createElement(
-//   "span",
-//   { id: "span-id" },
-//   "Hello React world",
-// );
 
-// const Btn = () => (
-//   <button
-//     onClick={() => {
-//       console.log("clicked");
-//     }}
-//   >
-//     Click me
-//   </button>
-// );
-// const btn = React.createElement(
-//   "button",
-//   {
-//     onClick: () => {
-//       console.log("clicked");
-//     },
-//   },
-//   "Click me",
-// );
-function Container() {
-  const [counter, setCounter] = React.useState(0);
-  const onClick = () => {
-    setCounter(counter + 1);
+function MinutesToHours() {
+  const [amount, setAmount] = React.useState();
+  const [MinutesToHours, setMinutesToHours] = React.useState(false);
+  const onChange = (e) => {
+    setAmount(e.target.value);
   };
-
+  const reset = (e) => {
+    setAmount(0);
+  };
+  const onMinutesToHours = (e) => {
+    reset();
+    setMinutesToHours((current) => !current);
+  };
   return (
     <div>
-      <h3>Total clicks: {counter}</h3>
-      <button onClick={onClick}>Click me</button>
+      <h1>Minutes & Hours</h1>
+      <div>
+        <label htmlFor="minutes">Minutes: </label>
+        <input
+          id="minutes"
+          placeholder="Minutes"
+          type="number"
+          value={MinutesToHours ? amount * 60 : amount}
+          onChange={onChange}
+          disabled={MinutesToHours}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="hours">Hours: </label>
+        <input
+          id="hours"
+          placeholder="Hours"
+          type="number"
+          value={MinutesToHours ? amount : Math.round(amount / 60)}
+          onChange={onChange}
+          disabled={!MinutesToHours}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onMinutesToHours}>
+        {MinutesToHours ? "Minutes" : "Hours"}
+      </button>
+    </div>
+  );
+}
+function KmToMiles() {
+  const [amount, setAmount] = React.useState();
+  const [KmToMiles, setKmToMiles] = React.useState(false);
+  const onChange = (e) => {
+    setAmount(e.target.value);
+  };
+  const reset = (e) => {
+    setAmount(0);
+  };
+  const onKmToMiles = (e) => {
+    reset();
+    setKmToMiles((current) => !current);
+  };
+  return (
+    <div>
+      <h1>Km & Miles</h1>
+      <div>
+        <label htmlFor="minutes">Km: </label>
+        <input
+          id="Km"
+          placeholder="Km"
+          type="number"
+          value={KmToMiles ? amount * 1.61 : amount}
+          onChange={onChange}
+          disabled={KmToMiles}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="hours">Miles: </label>
+        <input
+          id="Miles"
+          placeholder="Miles"
+          type="number"
+          value={KmToMiles ? amount : Math.round(amount / 1.61)}
+          onChange={onChange}
+          disabled={!KmToMiles}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onKmToMiles}>{KmToMiles ? "Km" : "Miles"}</button>
+    </div>
+  );
+}
+function App() {
+  const [index, setIndex] = React.useState("xx");
+  const onSelect = (e) => {
+    setIndex(e.target.value);
+  };
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="xx">Select your units</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & Miles</option>
+      </select>
+      <hr />
+      {index === "0" ? <MinutesToHours /> : null}
+      {index === "1" ? <KmToMiles /> : null}
     </div>
   );
 }
 
-ReactDOM.render(<Container />, root);
+ReactDOM.render(<App />, root);
 
 // function render() {
 //   ReactDOM.render(<Container />, root);
