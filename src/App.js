0f169cde9c943f5088ce5b6599d58bd95dd3d55f
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-08-27 16:17:29 +0900

    useEffect를 활용한 렌더링 제어 복습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import styles from "./css/App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("SEARCH FOR", keyword);
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        type="text"
+        placeholder="Search here..."
+        onChange={onChange}
+        value={keyword}
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"Click me"} onClick={onClick} />
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
 import styles from "./css/Button.module.css";
 
-function Button({ text }) {
-  return <button className={styles.btn}>{text}</button>;
+function Button({ text, onClick }) {
+  return (
+    <button className={styles.btn} onClick={onClick}>
+      {text}
+    </button>
+  );
 }
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default Button;
